2ee9e85aeec5cc3ae51b01e0925670c04570f8ab
caceco5 <cacerescarlosmauricio@gmail.com>
2020-08-04 14:39:37 -0500

    First commit of Platzi's Dinamic & Estocastic python coding

---
+++ camino_de_borrachos.py
@@ -0,0 +1,15 @@
+import random
+
+class Borracho:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+
+class BorrachoTradicional(Borracho):  # clase base, genera un contructor con init
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def camina(self): # camina aleatoriamente entre 4 opciones
+        return random.choice([(0,1), (0,-1), (1,0), (-1,0)]) #choice permite generar varias opciones que tienen la misma probabilidad
